d3e5c52b18dbdc710f2f18940059c836a0a536f5
Nicco <bresslerniccolo@gmail.com>
2022-01-09 03:55:26 -0500

    Removed unneeded import

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
 driver = webdriver.Chrome()
